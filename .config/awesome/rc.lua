d130713595b3c7ac73d48b4fd0d9c334ae33d366
Dmitry Astapenko <dmitry@colourbox.com>
2014-12-01 14:16:49 +0100

    Disable mouse wheen on tags

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -112,11 +112,11 @@ mylayoutbox = {}
 mytaglist = {}
 mytaglist.buttons = awful.util.table.join(
                     awful.button({ }, 1, awful.tag.viewonly),
-                    awful.button({ modkey }, 1, awful.client.movetotag),
-                    awful.button({ }, 3, awful.tag.viewtoggle),
-                    awful.button({ modkey }, 3, awful.client.toggletag),
-                    awful.button({ }, 4, awful.tag.viewnext),
-                    awful.button({ }, 5, awful.tag.viewprev)
+                    awful.button({ modkey }, 1, awful.client.movetotag)
+--                    awful.button({ }, 3, awful.tag.viewtoggle),
+--                    awful.button({ modkey }, 3, awful.client.toggletag),
+--                    awful.button({ }, 4, awful.tag.viewnext),
+--                    awful.button({ }, 5, awful.tag.viewprev)
                     )
 mytasklist = {}
 mytasklist.buttons = awful.util.table.join(
@@ -189,11 +189,9 @@ end
 -- }}}
 
 -- {{{ Mouse bindings
-root.buttons(awful.util.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
-))
+--root.buttons(awful.util.table.join(
+--    awful.button({ }, 3, function () mymainmenu:toggle() end)
+--))
 -- }}}
 
 -- {{{ Key bindings
