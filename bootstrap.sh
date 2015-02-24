fc062f64d5d720b731f92521a5acc2eddd63334d
Dmitry Astapenko <dmitry@colourbox.com>
2015-02-24 09:38:07 +0100

    Add todo for bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,15 @@
 # Install NeoBundle
 
+echo "Downloading NeoBundle"
+
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+
+# TODO
+#
+# Make ~/bin
+# Symlink all scripts to bin
+# Setup theme properly "git pull"
+#
+# Symlink all files
+# Check all config files and symlink them
+# Cleanup all unused stuff
