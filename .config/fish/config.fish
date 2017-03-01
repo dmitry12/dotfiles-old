05ed3417c78379ce531b1aa1b2adfdd6ff2ef04c
Dmitry Astapenko <dmitry.astapenko@gmail.com>
2017-03-01 14:55:46 +0100

    Add fish shell config

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+fish_vi_key_bindings
+
+alias vim="nvim"
+
+alias gs="git status"
+alias gc="git commit"
+alias gd="git diff"
+alias gdc="git diff --cached"
+alias ga="git add"
+alias gco="git checkout"
